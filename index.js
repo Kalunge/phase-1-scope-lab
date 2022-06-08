d9d5fa64beea07f5842fe8a88cc570165e875d48
kalunge <muthomititus+github@gmail.com>
2022-06-08 14:58:56 +0300

    Done with javascript scope assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toLocaleUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "titsu";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "sjsj";
+}
